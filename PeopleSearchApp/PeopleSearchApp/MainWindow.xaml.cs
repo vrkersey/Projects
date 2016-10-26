a7f9178801cf5dea19ee9fba9a567170be785341
Victor Kersey <vrkersey@gmail.com>
2016-10-26 08:26:04 -0600

    Everything Implemented, needs some further testing

--- PeopleSearchApp/PeopleSearchApp/MainWindow.xaml.cs
+++ PeopleSearchApp/PeopleSearchApp/MainWindow.xaml.cs
@@ -21,45 +21,68 @@ namespace PeopleSearchApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Database database;
-        private User searchedUser;
+        private Database database; // used to store all users
+        private User searchedUser; // the searched user
 
         public MainWindow()
         {
-            database = new Database();
-            addSomeUsers();
-
-            
+            database = new Database(); 
+            addSomeUsers(); //adds some default users to manipulate
 
             InitializeComponent();
 
-            foreach (User u in database.allUsers())
-                searchTxtBox.Items.Add(u.Name);
+            addMenuItem.Click += new RoutedEventHandler(Switch2User); // adds an event handler to the File -> Add Menu Item
+            searchMenuItem.Click += new RoutedEventHandler(Switch2Search); // adds an event handler to the File -> Search Menu Item
+            userImageBtn.Click += new RoutedEventHandler(importImage); // adds an event handler for the button that adds a picture to a user
+            closeMenuItem.Click += new RoutedEventHandler(closeWindow); // adds an event handler to the File -> Close Menu Item
 
-            addMenuItem.Click += new RoutedEventHandler(Switch2User);
-            searchMenuItem.Click += new RoutedEventHandler(Switch2Search);
-            userImageBtn.Click += new RoutedEventHandler(importImage);
+            // adds an event handler for when text changes in the Combo Box
             searchTxtBox.AddHandler(System.Windows.Controls.Primitives.TextBoxBase.TextChangedEvent, new System.Windows.Controls.TextChangedEventHandler(textTyped));
+            //searchTxtBox.IsDropDownOpen = true;
         }
 
+        // Event Handler for when text is typed in the Combo Box
         private void textTyped(object sender, RoutedEventArgs e)
         {
-            searchTxtBox.Items.Clear();
-            searchTxtBox.IsDropDownOpen = true;
-            List<User> list = new List<User>(database.prefix(searchTxtBox.Text));
-            if (list.Count == 1)
-                searchTxtBox.Text = list.First<User>().Name;
-            if (searchTxtBox.Text != null && list.Count!=0 && searchTxtBox.Text != "")
+            dropDownListBox.Items.Clear(); //removes all Items in the Combo Box
+
+            if (searchTxtBox.Text != null && searchTxtBox.Text != "")
             {
-                
-                foreach (User u in list)
-                    searchTxtBox.Items.Add(u.Name);
+                dropDownListBox.IsEnabled = true;
+                // a container to hold all elements that will be added to the ComboBox
+                List<User> list = new List<User>(database.prefix(searchTxtBox.Text));
+
+                //if (list.Count == 1) // if there is only one item in the container
+                //{
+                //    dropDownListBox.Items.Add(list.First<User>().Name);
+                //    searchBtn.Focus();
+                //}
+
+                // if there are more one item in the collection then add them to the comboBox
+                if (list.Count >= 1)
+                {
+                    foreach (User u in list)
+                    {
+                        ListBoxItem item = new ListBoxItem();
+                        //item.Name = u.FirstName + "." + u.LastName;
+                        item.Content = u.Name;
+                        item.MouseDoubleClick += listboxItemClick;
+                        dropDownListBox.Items.Add(item);
+                    }
+                }
             }
-            else if (searchTxtBox.Text == "")
-                foreach (User u in database.allUsers())
-                    searchTxtBox.Items.Add(u.Name);
+            else
+                dropDownListBox.IsEnabled = false;
         }
 
+        private void listboxItemClick(object sender, MouseButtonEventArgs e)
+        {
+            ListBoxItem item = (ListBoxItem)(sender);
+            searchTxtBox.Text = item.Content.ToString();
+            searchBtn_Click(sender, e);
+        }
+
+        // switch to the Search Screen and activate the Add menu button
         private void Switch2Search(Object sender, RoutedEventArgs e)
         {
             addMenuItem.IsEnabled = true;
@@ -68,6 +91,7 @@ namespace PeopleSearchApp
             userGrid.Visibility = Visibility.Hidden;
         }
 
+        //switch to the User screen and activate the Search menu button
         private void Switch2User(Object sender, RoutedEventArgs e)
         {
             addMenuItem.IsEnabled = false;
@@ -75,7 +99,7 @@ namespace PeopleSearchApp
             searchGrid.Visibility = Visibility.Hidden;
             userGrid.Visibility = Visibility.Visible;
 
-            if (sender.GetType() == typeof(MenuItem))
+            if (sender.GetType() == typeof(MenuItem))// if the event sender was the add menu button we are adding instead of editing
             {
                 fNameTxtBox.IsEnabled = true;
                 lNameTxtBox.IsEnabled = true;
@@ -83,14 +107,18 @@ namespace PeopleSearchApp
             }
         }
 
+        // Event handler for adding a picture to the user profile
         private void importImage(Object sender, RoutedEventArgs e)
         {
             OpenFileDialog newDialog = new OpenFileDialog();
-            newDialog.Filter = "Image files (*.bmp, *.gif, *.ico, *.jpg, *.png, *.wdp, *.tiff)|*.bmp; *.gif; *.ico; *.jpg; *.png; *.wdp; *.tiff|All files(*.*)|*.*";
+            
+            // acceptable file formats
+            newDialog.Filter = "Image files (*.bmp, *.gif, *.ico, *.jpg, *.png, *.wdp, *.tiff)|*.bmp; *.gif; *.ico; *.jpg; *.png; *.wdp; *.tiff";
             if (newDialog.ShowDialog() == true)
             {
                 try
                 {
+                    // make sure the picture will work
                     userPicture.Source = new BitmapImage(new Uri(newDialog.FileName));
                 }
                 catch
@@ -101,8 +129,11 @@ namespace PeopleSearchApp
             
         }
 
+        // Event handler for Search button. 
+        // Finds a user that has the
         private void searchBtn_Click(object sender, RoutedEventArgs e)
         {
+            dropDownListBox.IsEnabled = false;
             User user = database.searchUser(searchTxtBox.Text);
             if (user == null)
                 MessageBox.Show("Unable to find a user by the name " + searchTxtBox.Text);
@@ -157,14 +188,22 @@ namespace PeopleSearchApp
             interestTxtBox.Text = "";
             lNameTxtBox.Text = "";
             searchTxtBox.Text = "";
-            searchTxtBox.IsDropDownOpen = false;
+            //searchTxtBox.IsDropDownOpen = false;
             saveUser.Content = "Save";
             userPicture.Source = new BitmapImage(new Uri("pack://application:,,,/PeopleSearchApp;component/Resources/default.png"));
         }
+
+        private void closeWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
         private void addSomeUsers()
         {
             User u1 = new User("John", "Doe");
+            u1.Address = "123 State St";
             User u2 = new User("Jane", "Doe");
+            u2.Address = "321 Main St";
             User u3 = new User("Ricky", "Bobby");
             User u4 = new User("George", "Washington");
             database.addUser(u1);
@@ -172,5 +211,7 @@ namespace PeopleSearchApp
             database.addUser(u3);
             database.addUser(u4);
         }
+
+
     }
 }
