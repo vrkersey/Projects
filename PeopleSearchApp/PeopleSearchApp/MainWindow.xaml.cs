25430e489f5007dbf28bb76c8783f4fa477c5e87
Victor Kersey <vrkersey@gmail.com>
2016-10-26 16:25:22 -0600

    Finished Testing

--- PeopleSearchApp/PeopleSearchApp/MainWindow.xaml.cs
+++ PeopleSearchApp/PeopleSearchApp/MainWindow.xaml.cs
@@ -27,64 +27,64 @@ namespace PeopleSearchApp
         public MainWindow()
         {
             database = new Database(); 
-            addSomeUsers(); //adds some default users to manipulate
 
             InitializeComponent();
 
-            addMenuItem.Click += new RoutedEventHandler(Switch2User); // adds an event handler to the File -> Add Menu Item
-            searchMenuItem.Click += new RoutedEventHandler(Switch2Search); // adds an event handler to the File -> Search Menu Item
-            userImageBtn.Click += new RoutedEventHandler(importImage); // adds an event handler for the button that adds a picture to a user
-            closeMenuItem.Click += new RoutedEventHandler(closeWindow); // adds an event handler to the File -> Close Menu Item
+            addMenuItem.Click += Switch2User; // adds an event handler to the File -> Add Menu Item
+            searchMenuItem.Click += Switch2Search; // adds an event handler to the File -> Search Menu Item
+            userImageBtn.Click += importImage; // adds an event handler for the button that adds a picture to a user
+            closeMenuItem.Click += closeWindow; // adds an event handler to the File -> Close Menu Item
+            addUsersMenuItem.Click += AddUsersMenuItem_Click; // adds an event handler to the Application -> add default users Menu Item
+            aboutMenuItem.Click += AboutMenuItem_Click; // adds an event handler to the Application -> help Menu Item
 
             // adds an event handler for when text changes in the Combo Box
             searchTxtBox.AddHandler(System.Windows.Controls.Primitives.TextBoxBase.TextChangedEvent, new System.Windows.Controls.TextChangedEventHandler(textTyped));
-            //searchTxtBox.IsDropDownOpen = true;
+            
         }
 
         // Event Handler for when text is typed in the Combo Box
         private void textTyped(object sender, RoutedEventArgs e)
         {
             dropDownListBox.Items.Clear(); //removes all Items in the Combo Box
-
             if (searchTxtBox.Text != null && searchTxtBox.Text != "")
             {
-                dropDownListBox.IsEnabled = true;
+                dropDownListBox.Visibility = Visibility.Visible;
                 // a container to hold all elements that will be added to the ComboBox
                 List<User> list = new List<User>(database.prefix(searchTxtBox.Text));
-
-                //if (list.Count == 1) // if there is only one item in the container
-                //{
-                //    dropDownListBox.Items.Add(list.First<User>().Name);
-                //    searchBtn.Focus();
-                //}
-
-                // if there are more one item in the collection then add them to the comboBox
-                if (list.Count >= 1)
-                {
-                    foreach (User u in list)
+                if (list.Count > 0)
+                    foreach (User u in list) //Loop to add Users to the ListBox
                     {
                         ListBoxItem item = new ListBoxItem();
-                        //item.Name = u.FirstName + "." + u.LastName;
-                        item.Content = u.Name;
-                        item.MouseDoubleClick += listboxItemClick;
+                        item.Name = u.LastName + "_" + u.FirstName;
+                        item.Content = u.Name + "\t\tAge: " + u.Age + "\nAddress: " + u.Address;
+                        item.MouseDoubleClick += listboxItemClick; //adds the double click event handler to the ListBoxItem
                         dropDownListBox.Items.Add(item);
                     }
+                else
+                {
+                    ListBoxItem item = new ListBoxItem();
+                    item.Content = "-Click to Add-";
+                    item.MouseDoubleClick += Switch2User;
+                    dropDownListBox.Items.Add(item);
                 }
+
             }
             else
-                dropDownListBox.IsEnabled = false;
+                dropDownListBox.Visibility = Visibility.Hidden;
         }
 
+        // Event Handler for when a ListBoxItem is doubleclicked
         private void listboxItemClick(object sender, MouseButtonEventArgs e)
         {
             ListBoxItem item = (ListBoxItem)(sender);
-            searchTxtBox.Text = item.Content.ToString();
+            searchTxtBox.Text = item.Name.Replace("_", ", ");
             searchBtn_Click(sender, e);
         }
 
         // switch to the Search Screen and activate the Add menu button
         private void Switch2Search(Object sender, RoutedEventArgs e)
         {
+            clearTextBoxes();
             addMenuItem.IsEnabled = true;
             searchMenuItem.IsEnabled = false;
             searchGrid.Visibility = Visibility.Visible;
@@ -94,6 +94,7 @@ namespace PeopleSearchApp
         //switch to the User screen and activate the Search menu button
         private void Switch2User(Object sender, RoutedEventArgs e)
         {
+            clearTextBoxes();
             addMenuItem.IsEnabled = false;
             searchMenuItem.IsEnabled = true;
             searchGrid.Visibility = Visibility.Hidden;
@@ -130,7 +131,7 @@ namespace PeopleSearchApp
         }
 
         // Event handler for Search button. 
-        // Finds a user that has the
+        // Finds a user that has the same name
         private void searchBtn_Click(object sender, RoutedEventArgs e)
         {
             dropDownListBox.IsEnabled = false;
@@ -153,10 +154,11 @@ namespace PeopleSearchApp
             }
         }
 
+        // Event handler for save/add button
         private void saveUser_Click(object sender, RoutedEventArgs e)
         {
             int age;
-            if ((string)saveUser.Content == "Add")
+            if ((string)saveUser.Content == "Add") // add creates a new user
             {
                 searchedUser = new User(fNameTxtBox.Text, lNameTxtBox.Text);
                 searchedUser.Address = addressTxtBox.Text;
@@ -164,12 +166,12 @@ namespace PeopleSearchApp
                 searchedUser.PicFilename = userPicture.Source.ToString();
                 if (int.TryParse(ageTxtBox.Text, out age))
                     searchedUser.Age = age;
-                database.addUser(searchedUser);
                 searchedUser.PicFilename = userPicture.Source.ToString();
+                database.addUser(searchedUser);
             }
-            else
+            else //save uses the searched user
             {
-                searchedUser.Address = addressTxtBox.Text;
+                searchedUser.Address = addressTxtBox.Text; 
                 searchedUser.Interests = interestTxtBox.Text;
                 searchedUser.PicFilename = userPicture.Source.ToString();
                 if (int.TryParse(ageTxtBox.Text, out age))
@@ -180,6 +182,7 @@ namespace PeopleSearchApp
             Switch2Search(sender,e);
         }
 
+        // Helper method for clearing everything when switching active screens
         private void clearTextBoxes()
         {
             addressTxtBox.Text = "";
@@ -188,30 +191,71 @@ namespace PeopleSearchApp
             interestTxtBox.Text = "";
             lNameTxtBox.Text = "";
             searchTxtBox.Text = "";
-            //searchTxtBox.IsDropDownOpen = false;
+            dropDownListBox.Items.Clear();
             saveUser.Content = "Save";
             userPicture.Source = new BitmapImage(new Uri("pack://application:,,,/PeopleSearchApp;component/Resources/default.png"));
         }
 
+        // Event handler for CloseMenuItem
         private void closeWindow(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        // Event handler for HelpMenuItem
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Adding a new User: click File -> Add \n\n"+
+                            "Searching for a User: Type in the first or last name of the User then double click on the correct user \n\n"+
+                            "Need more help? seek the README.txt File");
+        }
+
+        // Event handler for addDefaultUsers which just adds some users
+        private void AddUsersMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            addSomeUsers();
+            addManyUsers();
+        }
+
+        // Adds 4 default users
         private void addSomeUsers()
         {
             User u1 = new User("John", "Doe");
-            u1.Address = "123 State St";
-            User u2 = new User("Jane", "Doe");
-            u2.Address = "321 Main St";
-            User u3 = new User("Ricky", "Bobby");
-            User u4 = new User("George", "Washington");
+            u1.Address = "Unkown";
+            u1.Age = 0;
+            u1.Interests = "Being Mysterious";
             database.addUser(u1);
+
+            User u2 = new User("Jane", "Doe");
+            u2.Address = "Unkown";
+            u2.Age = 0;
+            u2.Interests = "Being Mysterious";
             database.addUser(u2);
+
+            User u3 = new User("Kimble", "Rod");
+            u3.Age = 19;
+            u3.Address = "2020 State St";
+            u3.Interests = "Partying, Stuntman";
             database.addUser(u3);
+
+            User u4 = new User("Ron", "Burgundy");
+            u4.Address = "123 Main St, San Diego";
+            u4.Age = 42;
+            u4.Interests = "Being lead Anchorman for Channel 4";
             database.addUser(u4);
+
         }
 
-
+        // Adds Alot of users, Used for testing
+        private void addManyUsers()
+        {
+            int numberOfUsers = 10000;
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                User u = new User(((char)(66 + i % 25)).ToString(), "LastName");
+                u.Age = i;
+                database.addUser(u);
+            }
+        }
     }
 }
---
+++ PeopleSearchApp/Unit Tests/UnitTest1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PeopleSearchApp;
+using System.Collections.Generic;
+
+namespace Unit_Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        #region UserTests
+
+        [TestMethod]
+        public void HashCodeTest()
+        {
+            User u1 = new User("firstName","lastName");
+            User u2 = new User("firstName", "lastname");
+            Assert.IsFalse(u1.GetHashCode().Equals(u2.GetHashCode()));
+        }
+
+        [TestMethod]
+        public void EqualityTest()
+        {
+            User u1 = new User("firstName", "lastName");
+            User u2 = new User("firstName", "lastName");
+            Assert.IsTrue(u1.Equals(u2));
+
+            u1.Age = 25;
+            u2.Age = 25;
+            Assert.IsTrue(u1.Equals(u2));
+
+            u1.Interests = "some stuff";
+            u2.Interests = "other stuff";
+            Assert.IsTrue(u1.Equals(u2));
+
+            u1.Address = "123 Main St";
+            u2.Address = "123 State St";
+            Assert.IsFalse(u1.Equals(u2));
+        }
+
+        #endregion
+
+        #region DatabaseTests
+
+        [TestMethod]
+        public void EmptyDatabaseTest()
+        {
+            Database db = new Database();
+            HashSet<User> set = new HashSet<User>(db.allUsers());
+            Assert.AreEqual(set.Count,0);
+
+        }
+
+        [TestMethod]
+        public void addUserTest()
+        {
+            Database db = new Database();
+            db.addUser(new User("firstName", "lastName"));
+            HashSet<User> set = new HashSet<User>(db.allUsers());
+            Assert.AreEqual(set.Count, 1);
+        }
+
+        [TestMethod]
+        public void searchUser()
+        {
+            string firstName = "firstName";
+            string lastName = "lastName";
+            Database db = new Database();
+            User newUser = new User(firstName, lastName);
+            db.addUser(newUser);
+
+            User returnedUser = db.searchUser(lastName + ", " + firstName);
+            Assert.IsTrue(returnedUser.Equals(newUser));
+        }
+
+        [TestMethod]
+        public void searchByPrefix()
+        {
+            User u1 = new User("AFirstName", "BLastName");
+            User u2 = new User("BFirstName", "CLastName");
+            Database db = new Database();
+            db.addUser(u1);
+            db.addUser(u2);
+            HashSet<User> set1 = new HashSet<User>(db.prefix("a"));
+            HashSet<User> set2 = new HashSet<User>(db.prefix("B"));
+            HashSet<User> set3 = new HashSet<User>(db.prefix("z"));
+
+            Assert.AreEqual(set1.Count, 1);
+            Assert.AreEqual(set2.Count, 2);
+            Assert.AreEqual(set3.Count, 0);
+        }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            Database db = new Database();
+            int numberOfUsers = 10000;
+            for (int i = 0; i < numberOfUsers; i++)
+            {
+                User u = new User(((char)(66 + i % 25)).ToString(), "LastName");
+                u.Age = i;
+                db.addUser(u);
+            }
+            HashSet<User> set1 = new HashSet<User>(db.prefix("a"));
+            HashSet<User> set2 = new HashSet<User>(db.prefix("B"));
+            HashSet<User> set3 = new HashSet<User>(db.prefix("LastName"));
+
+            Assert.AreEqual(set1.Count, 0);
+            Assert.AreEqual(set2.Count, numberOfUsers/25);
+            Assert.AreEqual(set3.Count, numberOfUsers);
+        }
+
+        #endregion
+    }
+}
