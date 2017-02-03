56688af06da2091cdb4b7a85632dc2a719213a1b
Victor Kersey <vrkersey@gmail.com>
2017-02-03 06:14:52 -0700

    PS3

---
+++ Algorithms/PS3/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            string[] input = Console.ReadLine().Split(' ');
+
+            long distanceSQRD = (long)Math.Pow(int.Parse(input[0]),2);
+            int k = int.Parse(input[1]);
+
+            HashSet<galaxy> possible = new HashSet<galaxy>();
+            HashSet<star> allstars = new HashSet<star>();
+
+            for (int i = 1; i <= k/2; i++)
+            {
+                input = Console.ReadLine().Split(' ');
+                x = int.Parse(input[0]);
+                y = int.Parse(input[1]);
+                star s1 = new star(x, y);
+
+                input = Console.ReadLine().Split(' ');
+                x = int.Parse(input[0]);
+                y = int.Parse(input[1]);
+                star s2 = new star(x, y);
+                allstars.Add(s1);
+                allstars.Add(s2);
+
+                if (s1.distanceSQRD(s2) <= distanceSQRD)
+                {
+                    galaxy g = new galaxy(s1, distanceSQRD);
+                    g.addStar(s2);
+                    possible.Add(g);
+                }
+            }
+            if (k%2 == 1)
+            {
+                input = Console.ReadLine().Split(' ');
+                x = int.Parse(input[0]);
+                y = int.Parse(input[1]);
+                star s1 = new star(x, y);
+                allstars.Add(s1);
+                possible.Add(new galaxy(s1, distanceSQRD));
+            }
+
+            Console.WriteLine(majorElement(possible, allstars));
+            Console.Read();
+        }
+
+        private static string majorElement(HashSet<galaxy> g, HashSet<star> stars)
+        {
+            int k = stars.Count;
+            if (g.Count == 0)
+            {
+                return "No";
+            }
+            else
+            {
+                foreach (galaxy gel in g)
+                {
+                    foreach (star sel in stars)
+                    {
+                        if (gel.testStar(sel))
+                        {
+                            gel.addStar(sel);
+                        }
+                    }
+
+                    if (gel.count > k / 2)
+                    {
+                        return gel.count.ToString();
+                    }
+                }
+            }
+            return "No";
+        }
+
+        private class galaxy
+        { 
+            private long distanceSQRD;
+            star firststar;
+            public int count;
+            HashSet<star> stars = new HashSet<star>();
+
+            public galaxy(star firststar, long distanceSQRD)
+            {
+                count = 1;
+                this.firststar = firststar;
+                stars.Add(firststar);
+                this.distanceSQRD = distanceSQRD;
+            }
+
+            public void addStar(star s)
+            {
+                stars.Add(s);
+                count++;
+            }
+
+            public bool testStar(star s)
+            {
+                return firststar.distanceSQRD(s) <= distanceSQRD;
+            }
+        }
+
+        private class star
+        {
+            public int x, y;
+            public star(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            public long distanceSQRD(star s)
+            {
+                return (long)Math.Pow(Math.Abs(x-s.x),2) + (long)Math.Pow(Math.Abs(y-s.y),2);
+            }
+        }
+    }
+}
+
+#if false
+
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 2, 5, 7, 22};
+            int[] B = new int[] { 7, 8, 9, 14};
+            int k = 0;
+            Console.WriteLine(select(A,B,k));
+            Console.Read();
+        }
+
+        static int select(int[] A, int [] B, int k)
+        {
+            return select(A, 0, A.Length - 1, B, 0, B.Length - 1, k);
+        }
+
+
+        static int select(int[] A, int loA, int hiA, int[] B, int loB, int hiB, int k)
+        {
+            // A[loA..hiA] is empty
+            if (hiA < loA)
+                return B[k - loA];
+
+            // B[loB..hiB] is empty
+            if (hiB < loB)
+                return A[k - loB];
+
+            // Get the midpoints of A[loA..hiA] and B[loB..hiB]
+            int i = (loA + hiA) / 2;
+            int j = (loB + hiB) / 2;
+
+            // Figure out which one of four cases apply
+            if (A[i] > B[j]) 
+            {
+                if (k <= i + j) 
+                    return select(A, loA, i - 1, B, loB, hiB, k);
+
+                else //correct
+                    return select(A, loA, hiA, B, j + 1, hiB, k);
+
+            }
+            else 
+            {
+                if (k <= i + j)
+                    return select(A, loA, hiA , B, loB, j - 1, k);
+
+                else  //correct
+                    return select(A, i + 1, hiA, B, loB, hiB, k);
+
+            }
+            
+        }
+#endif
+
---
+++ Algorithms/tester/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static int dec2bin(string x)
+        {
+            int n = x.Length;
+            if (n == 1)
+                return binary(int.Parse(x));
+            else
+            {
+                return dec2bin(x.Substring(0, n / 2)) * pow2bin(n / 2) + dec2bin(x.Substring(n / 2));
+            }
+            //return 0;
+        }
+
+        static int binary(int x)
+        {
+            switch (x)
+            {
+                case 0:
+                    return 0000;
+                case 1:
+                    return 0001;
+                case 2:
+                    return 0010;
+                case 3:
+                    return 0011;
+                case 4:
+                    return 0100;
+                case 5:
+                    return 0101;
+                case 6:
+                    return 0110;
+                case 7:
+                    return 0111;
+                case 8:
+                    return 1000;
+                case 9:
+                    return 1001;
+            }
+            return 0;
+        }
+//        function dec2bin(x)
+//        if n = 1: return binary[x]
+//        else:
+//        split x into two decimal numbers xL, xR with n=2 digits each
+//        return ???
+    }
+}
